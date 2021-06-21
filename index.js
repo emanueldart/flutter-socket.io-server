e8bceee57247f1e0c5db4650a60761318f340a86
emanueldart <https://github.com/emanueldart/flutter-socket.io-server.git>
2021-06-20 23:15:47 -0300

    socket terminado

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const express= require('express');
-//app de express
-const app = express();
-
+const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-//creo el servidor de soquet
+// App de Express
+const app = express();
+
+// Node Server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
-
-
-//llamo a socket
-require("./socket/socket");
+require('./socket/socket');
 
 
 
-// path publico 
-const publicPath = path.resolve(__dirname,'public');
-app.use( express.static(publicPath));
 
-server.listen( process.env.PORT, (err)=>{
-    if (err) throw new Error(err);
-    console.log('Servidor corriendo en puerto!',process.env.PORT);
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+
+
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
 });
+
+
--- socket/socket.js
+++ socket/socket.js
@@ -1,17 +1,54 @@
-const {io}= require("../index");
+const { io } = require('../index');
+
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band( 'Breaking Benjamin' ) );
+bands.addBand( new Band( 'Bon Jovi' ) );
+bands.addBand( new Band( 'Héroes del Silencio' ) );
+bands.addBand( new Band( 'Metallica' ) );
 
 
-// mensajes de sockets
-io.on('connection',client =>{
-    console.log("cliente conectado");
-    client.on("disconnect",()=> {
-        console.log('cliente desconectado');
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands() );
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
     });
-    client.on('mensaje',(payload)=>{
-        console.log('mensjaeee!',payload);
 
-        //con io emito el servido emite un mensaje a todos los clientes conectados
-        io.emit("mensaje",{admin:"nuevo mensaje"});
+    client.on('mensaje', ( payload ) => {
+        console.log('Mensaje', payload);
+        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
     });
+
+    client.on('vote-band', (payload) => {
+
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band( payload.name );
+        bands.addBand( newBand );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band', (payload) => {
+
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    // client.on('emitir-mensaje', ( payload ) => {
+    //     // console.log(payload);
+    //     // io.emit('nuevo-mensaje', payload ); // emite a todos!
+    //     client.broadcast.emit('nuevo-mensaje', payload ); // emite a todos menos el que lo emitió
+    // })
+
+
 });
-
